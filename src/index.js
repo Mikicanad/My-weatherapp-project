893ab081bab63d9a12dff016a0af6b0ba853aa42
Mikicanad <mmmcf5290@gmail.com>
2024-08-27 14:05:13 -0700

    added search engine

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 function handleASearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    console.log(searchInput.value);
+    let cityElement = document.querySelector("#city")
+    cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
