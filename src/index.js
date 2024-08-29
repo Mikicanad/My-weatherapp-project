ea515174eaa5399d4be3090ad2b1c52dbbddf32a
Mikicanad <mmmcf5290@gmail.com>
2024-08-28 22:30:56 -0700

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+let apiKey = "3c0eca18f3a2743b8tdo8c93b0a6483f"
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleASearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city")
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleASearchSubmit);
+
+searchCity("Vancouver");
