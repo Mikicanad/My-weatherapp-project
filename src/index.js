30ce42276824867a4742d8fce9736f6ecdc3f8fa
Mikicanad <mmmcf5290@gmail.com>
2024-09-02 21:12:42 -0700

    Inject the forecast HTML from Jacascript

--- src/index.js
+++ src/index.js
@@ -55,7 +55,35 @@ function handleASearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+
+function displayForecast(){
+   
+let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+let forecastHtml = "";
+
+days.forEach(function(day) {
+forecastHtml = 
+   forecastHtml +
+   `
+   <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤</div>
+    <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature"><strong>15°C</strong></div>
+    <div class="weather-forecast-temperature">9°C</div>
+   </div>
+ </div>
+  `;
+});
+
+ let forecastElement = document.querySelector("#forecast");
+
+forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleASearchSubmit);
 
 searchCity("Vancouver");
+displayForecast();
+
